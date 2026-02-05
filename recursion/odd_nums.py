0a0adb7a9d7d0c837618126cab3bb58a38b15eba
Bonnie Owens <arlolocos@gmail.com>
2026-02-05 16:20:22 -0500

    Add recursion helper logic

---
+++ recursion/odd_nums.py
@@ -0,0 +1,34 @@
+def get_odd_nums(arr, odds = []):
+    end_index = len(arr) -1
+    if len(arr) == 0:
+        return odds
+
+    if arr[end_index] % 2 == 1:
+        odds.append(arr[end_index])
+    arr.pop(end_index)
+
+    return get_odd_nums(arr, odds)
+
+
+print(get_odd_nums([1,4,5,3,6,8,9]))
+
+
+def get_odds_helper(arr):
+    odds = []
+
+    def sort_nums(helper_arr):
+        length = len(helper_arr)
+        if length == 0:
+            return
+
+        if helper_arr[length - 1] % 2 == 1:
+            odds.append(helper_arr[length -1])
+
+        helper_arr.pop(length - 1)
+
+        sort_nums(helper_arr)
+
+    sort_nums(arr)
+    return odds
+
+print(get_odds_helper([1,4,5,3,6,8,9]))
