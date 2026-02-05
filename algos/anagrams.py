86c930af7fb0aeafb777e56a6054affff9f070a1
Bonnie Owens <arlolocos@gmail.com>
2026-02-04 21:45:15 -0500

    Simplify loops

--- algos/anagrams.py
+++ algos/anagrams.py
@@ -1,6 +1,5 @@
 def anagram(s1, s2):
     s1_cnt = {}
-    s2_cnt = {}
 
     if len(s1) != len(s2):
         return False
@@ -9,9 +8,14 @@ def anagram(s1, s2):
         s1_cnt[c] = s1_cnt.get(c, 0) + 1
 
     for c in s2:
-        s2_cnt[c] = s2_cnt.get(c, 0) + 1
+        if c in s1_cnt:
+            s1_cnt[c] -= 1
+            if s1_cnt[c] == 0:
+                del s1_cnt[c]
+        else:
+            return False
 
-    return s1_cnt == s2_cnt
+    return s1_cnt == {}
 
 print(anagram("cinema", "iceman"))
 print(anagram("hello", "world"))
